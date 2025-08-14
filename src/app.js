13110022dbaf3385316d25253b43a34e00a4e145
Matheus M Dach <dachmatheus@gmail.com>
2025-08-13 23:37:18 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const path = require('path');
 const db = require('./config/db');
 
 const usuariosRoutes = require('./routes/usuarios');
@@ -10,27 +9,27 @@ const resultadoRoutes = require('./routes/resultado');
 const authRoutes = require('./routes/auth');
 const vincularRoute = require('./routes/vincular');
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Rotas da API prefixadas com /api
+// ROTAS prefixadas com /api
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/respostas', respostasRoutes);
 app.use('/api/resultado', resultadoRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/vincular', vincularRoute);
 
-// Servir arquivos estáticos da pasta public
-app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve index.html para qualquer rota que não comece com /api
-app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+// Rota de teste
+app.get('/', (req, res) => {
+    res.send('API DISC funcionando!');
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
